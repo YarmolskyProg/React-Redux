7f2640d5de3ed5213538216dc73703f8d9ca633a
Yarmolsky <vadym.yarmolsky.prog@gmail.com>
2022-10-16 12:39:42 +0300

    quis is ready

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './index.scss';
 
 const questions = [
@@ -22,37 +23,54 @@ const questions = [
   },
 ];
 
-function Result() {
+function Result({correct}) {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
+      <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
+      <a href='/'>Попробовать снова</a>
     </div>
   );
 }
 
-function Game() {
+function Game({ obj, question, handlerClick }) {
+
+  let per = 0
+  per = Math.round((question) * 100 / questions.length)
   return (
     <>
       <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
+        <div style={{ width: `${per}%` }} className="progress__inner"></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{obj.title}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+        {obj.variants.map((res, index) =>
+          <li onClick={() => handlerClick(index)} key={index}>{res}</li>
+        )}
       </ul>
     </>
   );
 }
 
 function App() {
+  const [question, setquestion] = useState(0);
+  const [correct, setcorrect] = useState(0);
+  function handlerClick(index) {
+    setquestion(question + 1)
+    if (questions[question].correct == index) {
+      setcorrect(correct + 1)
+
+    }
+
+  }
   return (
     <div className="App">
-      <Game />
-      {/* <Result /> */}
+      {
+        question !== questions.length ?
+          <Game obj={questions[question]} question={question} handlerClick={handlerClick} /> :
+          <Result correct={correct} />
+      }
+
     </div>
   );
 }

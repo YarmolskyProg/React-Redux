7151a20de76924def03b9d798b01aa2ec49b6a54
Yarmolsky <vadym.yarmolsky.prog@gmail.com>
2022-10-16 11:41:52 +0300

    modal

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-
 import React from 'react';
-import { useState } from 'react';
 import './index.scss';
 
 function App() {
-  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>{counter}</h1>
-        <button onClick={() => (setCounter(counter - 1))} className="minus">- Минус</button>
-        <button onClick={() => (setCounter(counter + 1))} className="plus">Плюс +</button>
-      </div>
+      <button className="open-modal-btn">✨ Открыть окно</button>
+      {/* <div className="overlay">
+        <div className="modal">
+          <svg height="200" viewBox="0 0 200 200" width="200">
+            <title />
+            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+          </svg>
+          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+        </div>
+      </div> */}
     </div>
   );
 }

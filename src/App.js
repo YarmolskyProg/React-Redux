3317a715b52c7039deb9238952f540b458a7ddbe
Yarmolsky <vadym.yarmolsky.prog@gmail.com>
2022-10-17 18:12:23 +0300

    users is ready

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
+import axios from 'axios';
 
 // Тут список пользователей: https://reqres.in/api/users
+let initial = axios.create({
+  baseURL: 'https://reqres.in/api/users'
+})
 
 function App() {
+  const [users, setusers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [invitelist, setinvitelist] = useState([]);
+  const [result, setresult] = useState(true);
+  const [count, setcount] = useState(0);
+  useEffect(() => {
+
+    initial.get().then(res => {
+      setusers(res.data.data)
+    })
+
+    setLoading(false)
+  }, [])
+  function onInvite(id) {
+    if (invitelist.includes(id)) {
+      setinvitelist(prev => prev.filter(_id => _id !== id))
+    }
+    else {
+      setinvitelist(prev => [...prev, id])
+    }
+    
+  }
+  const [search, setsearch] = useState('');
+  function handlerChange(e) {
+    setsearch(e.target.value)
+  }
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {result ?
+        <Users setresult={setresult} items={users} search={search} invitelist={invitelist} isLoading={isLoading} handlerChange={handlerChange} onInvite={onInvite} /> :
+        <Success count={invitelist.length}/>}
     </div>
   );
 }
